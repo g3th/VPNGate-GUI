366836abbc5d2399c295c0a43c8303bb22c58a7d
g3th <theinkandpen@yahoo.co.uk>
2022-07-13 07:25:02 +0100

    modified:   config_download.py 	modified:   vpngate.py 	graphical_elements.py

--- config_download.py
+++ config_download.py
@@ -3,6 +3,8 @@ import servers
 import shutil
 import os
 
+from tkinter import *
+from tkinter import ttk
 from servers import download_ovpn_config
 
 open_vpn = download_ovpn_config()
@@ -11,35 +13,45 @@ os.makedirs('config')
 
 class download_ovpn_configuration_files:
 
-	def __init__(self):
+	def __init__(self,root):
 		
 		self.vpngate = 'https://www.vpngate.net/en/'
 		self.futures_list=[]
 		self.downloading_urls=[]
 		self.configuration_number=0
+		self.progressbar = ttk.Progressbar(root, orient = 'horizontal', mode = 'determinate', length = 280)
+		self.progressbar.pack(padx=20,pady=140)#grid(column=0, row=0, columnspan=2, padx=100, pady=100)
 		
-	def create_ovpn_download_list(self):
-	
-		with open('config/openvpn_config_url_list','a') as openvpn_urls:
+	def create_ovpn_download_list(self,root):
+		root.update_idletasks()		
+		with open('config/openvpn_config_url_list','a') as openvpn_urls:	
 			for link in open_vpn.get_all_page_links():
+				self.progressbar.start()				
 				openvpn_urls.write(self.vpngate + link + "\n")
+				self.progressbar['value']+=1
+				self.progressbar.stop()
 		openvpn_urls.close()
-
 		
-	def download_ovpn_files(self):		
-
+	def download_ovpn_files(self, root):
+		root.update_idletasks()
 		with concurrent.futures.ThreadPoolExecutor() as executor:
 				
 				with open('config/openvpn_config_url_list','r') as configs:
 					
 					for line in configs.readlines():
 						thread = executor.submit(servers.get_ovpn_configuration_link, line)
+						self.progressbar['value']+=1
 						self.futures_list.append(thread)
-					
+
+						
 					for returned_value in self.futures_list:
 						result = returned_value.result()
+						self.progressbar['value']+=1
 						self.downloading_urls.append(result)
 						
 					for url in self.downloading_urls:
-						executor.submit(servers.download_config, url, str(self.configuration_number))
+						executor.submit(servers.download_config, url, str(self.configuration_number))	
 						self.configuration_number += 1
+						self.progressbar['value']+= 1
+						self.progressbar.stop()
+						
--- vpngate.py
+++ vpngate.py
@@ -2,8 +2,7 @@ import config_download as config
 
 from tkinter import *
 from tkinter import ttk
-from PIL import Image, ImageTk
-
+from graphical_elements import interface_graphical_elements as graphics
 
 class vpngate_client:
 
@@ -13,27 +12,22 @@ class vpngate_client:
 		self.vpngate_gui.title('VPN Gate Client')
 		self.vpngate_gui.resizable(False,False)
 		self.vpngate_gui.geometry('375x450')
+		self.configs = config.download_ovpn_configuration_files(self.vpngate_gui)
 		
-	def gui_logo(self):
-		open_image = (Image.open('logo.jpg'))
-		resized= open_image.resize((363,100))
-		image = ImageTk.PhotoImage(resized)		
-		canvas = Canvas(self.vpngate_gui, width = 100, height = 100)
-		canvas.image = image
-		canvas.create_image(5,5,anchor=NW,image=image)
-		canvas.place(x=1,y=1)
+	def elements(self):
+	
+		graphics().gui_logo(self.vpngate_gui)
 		
-	def create_lists_download_configs(self):
-		configs = config.download_ovpn_configuration_files()
-		configs.create_ovpn_download_list()
-		message_label = Label(self.vpngate_gui, text = configs.download_ovpn_files())
-		message_label.pack(padx=30, pady=100)
+	def download(self):
+		
+		self.configs.create_ovpn_download_list(self.vpngate_gui)		
+		self.configs.download_ovpn_files(self.vpngate_gui)
 		
 	def WindowLoop(self):
 		
 		self.vpngate_gui.mainloop()
-		
+	
 vpngate = vpngate_client()
-vpngate.create_lists_download_configs()
-vpngate.gui_logo()
+vpngate.elements()
+vpngate.download()
 vpngate.WindowLoop()
